873f54b3e2689383167644d910492135f9de4f54
Makoto <dimonkrupko@yandex.ru>
2024-11-25 00:59:37 +0300

    add/number_dict

--- numbers.py
+++ numbers.py
@@ -1,6 +1,11 @@
 X = int(input('Введите число: '))  # type: ignore
 number_list = list(range(1, X+1))
-#number_rev_list = number_list.reverse()
 number_rev_list = list(reversed(number_list))
 print(number_list)
-print(number_rev_list)
+number_dict = {}
+for key in number_list:
+    for value in number_rev_list:
+        number_dict[key] = value
+        number_rev_list.remove(value)
+        break
+print(number_dict)
