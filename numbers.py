6736924b7d6bb51389268e711442d20f59ddf0ba
Makoto <dimonkrupko@yandex.ru>
2024-11-25 00:55:12 +0300

    add/reverse_list

--- numbers.py
+++ numbers.py
@@ -1,3 +1,6 @@
 X = int(input('Введите число: '))  # type: ignore
 number_list = list(range(1, X+1))
+#number_rev_list = number_list.reverse()
+number_rev_list = list(reversed(number_list))
 print(number_list)
+print(number_rev_list)
