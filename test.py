4b6a658a1bd56566595a2b967727fbb003dbaa01
Makoto <dimonkrupko@yandex.ru>
2024-11-28 19:04:49 +0300

    replace

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-X = int(input('Введите число: '))  # type: ignore
+X = int(input('Введите число: ')) 
 number_list = list(range(1, X+1))
 number_rev_list = list(reversed(number_list))
 print(number_list)
